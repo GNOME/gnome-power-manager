a87611033bad0c5d3ad4f2d7b9e3edfada41eef2
Richard Hughes <rhughes@src.gnome.org>
2005-08-26 23:09:47 +0000

    Update FAQ for kernel video problems.

--- docs/faq.php
+++ docs/faq.php
@@ -26,7 +26,7 @@ The DBUS interface API is changing <b>frequently</b>, and will continue to do so
 Many DBUS apps have lots of configure logic and extra code that I could not maintain - and is a waste of my time to support a version of DBUS that is already obsolete.
 I will always track the latest released version.
 Also, some bugs in the newer DBUS's have been fixed that impact the operation of GNOME Power Manager.
-You are welcome to patch G-P-M to work with an older DBUS is you wish.
+You are welcome to patch G-P-M to work with an older DBUS if you wish.
 </p>
 
 <hr>
@@ -37,6 +37,15 @@ Why do we need such an up-to-date HAL?
 G-P-M uses advanced features of HAL that <b>did not exist</b> in past versions.
 You cannot patch G-P-M to support an older HAL, as it *will not* work.
 Encourage your distro to update their version of HAL, and then G-P-M will work.
+</p>
+
+<hr>
+<p class="faqquestion">
+Why doesn't my video adaptor come back after a suspend? It just displays black.
+</p>
+<p class="faqanswer">
+You may need to add a s3 command to your kernel boot string so that the kernel can re-initialise your video card.
+See <a href="data/video.txt">this document</a> for more details, or to see if your system has been identified as needing the switch.
 </p>
 
 <?php
