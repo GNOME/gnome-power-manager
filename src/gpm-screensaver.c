2196db21ed67d5a6a39bd2b763ae4bf576b021d4
Richard Hughes <rhughes@src.gnome.org>
2005-09-15 12:36:40 +0000

    New files for the GNOME Screensaver stuff.

---
+++ src/gpm-screensaver.c
@@ -0,0 +1,130 @@
+/***************************************************************************
+ *
+ * glibhal-extras.c : GLIB replacement for libhal, the extra stuff
+ *
+ * Copyright (C) 2005 Richard Hughes, <richard@hughsie.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ **************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <glib.h>
+#include <dbus/dbus-glib.h>
+
+#include "dbus-common.h"
+#include "glibhal-main.h"
+#include "gpm-screensaver.h"
+
+/** If set to lock on screensave, instruct gnome-screensaver to lock screen
+ *  and return TRUE.
+ *  if set not to lock, then do nothing, and return FALSE.
+ */
+gboolean
+gscreensaver_set_throttle (gboolean throttle)
+{
+	GError *error = NULL;
+	DBusGConnection *session_connection;
+	DBusGProxy *gs_proxy;
+	gboolean boolret;
+
+	dbus_get_session_connection (&session_connection);
+	gs_proxy = dbus_g_proxy_new_for_name (session_connection,
+			GS_LISTENER_SERVICE,
+			GS_LISTENER_PATH,
+			GS_LISTENER_INTERFACE);
+	if (!dbus_g_proxy_call (gs_proxy, "setThrottleEnabled", &error,
+				G_TYPE_BOOLEAN, throttle, G_TYPE_INVALID,
+				G_TYPE_BOOLEAN, &boolret, G_TYPE_INVALID)) {
+		dbus_glib_error (error);
+		g_debug ("gnome-screensaver service is not running.");
+		boolret = FALSE;
+	}
+	g_object_unref (G_OBJECT (gs_proxy));
+	if (!boolret) {
+		g_debug ("gnome-screensaver setThrottleEnabled failed");
+		return FALSE;
+	}
+	return TRUE;
+}
+
+/** If set to lock on screensave, instruct gnome-screensaver to lock screen
+ *  and return TRUE.
+ *  if set not to lock, then do nothing, and return FALSE.
+ */
+gboolean
+gscreensaver_lock (void)
+{
+	GError *error = NULL;
+	DBusGConnection *session_connection;
+	DBusGProxy *gs_proxy;
+	gboolean boolret;
+
+	dbus_get_session_connection (&session_connection);
+	gs_proxy = dbus_g_proxy_new_for_name (session_connection,
+			GS_LISTENER_SERVICE,
+			GS_LISTENER_PATH,
+			GS_LISTENER_INTERFACE);
+	if (!dbus_g_proxy_call (gs_proxy, "lock", &error,
+				G_TYPE_INVALID,
+				G_TYPE_BOOLEAN, &boolret, G_TYPE_INVALID)) {
+		dbus_glib_error (error);
+		g_debug ("gnome-screensaver service is not running.");
+		boolret = FALSE;
+	}
+	g_object_unref (G_OBJECT (gs_proxy));
+	if (!boolret) {
+		g_debug ("gnome-screensaver lock failed");
+		return FALSE;
+	}
+	return TRUE;
+}
+
+/** If set to lock on screensave, instruct gnome-screensaver to lock screen
+ *  and return TRUE.
+ *  if set not to lock, then do nothing, and return FALSE.
+ */
+gboolean
+gscreensaver_get_idle (void)
+{
+	GError *error = NULL;
+	DBusGConnection *session_connection;
+	DBusGProxy *gs_proxy;
+	gboolean boolret = TRUE;
+	gboolean value;
+
+	dbus_get_session_connection (&session_connection);
+	gs_proxy = dbus_g_proxy_new_for_name (session_connection,
+			GS_LISTENER_SERVICE,
+			GS_LISTENER_PATH,
+			GS_LISTENER_INTERFACE);
+	if (!dbus_g_proxy_call (gs_proxy, "getIdle", &error,
+				G_TYPE_INVALID,
+				G_TYPE_BOOLEAN, &value, G_TYPE_INVALID)) {
+		dbus_glib_error (error);
+		g_debug ("gnome-screensaver service is not running.");
+		boolret = FALSE;
+	}
+/*g_print ("gscreensaver_get_idle: %i\n", value);*/
+	g_object_unref (G_OBJECT (gs_proxy));
+	if (!boolret) {
+		g_debug ("gnome-screensaver get idle failed");
+		return FALSE;
+	}
+	return TRUE;
+}
---
+++ src/gpm-screensaver.h
@@ -0,0 +1,33 @@
+/***************************************************************************
+ *
+ * glibhal-extras.h : GLIB replacement for libhal, the extra stuff
+ *
+ * Copyright (C) 2005 Richard Hughes, <richard@hughsie.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ **************************************************************************/
+
+#ifndef _GPMSCREENSAVER_H
+#define _GPMSCREENSAVER_H
+
+#define GS_LISTENER_SERVICE	"org.gnome.screensaver"
+#define GS_LISTENER_PATH	"/org/gnome/screensaver"
+#define GS_LISTENER_INTERFACE	"org.gnome.screensaver"
+
+gboolean gscreensaver_lock (void);
+gboolean gscreensaver_set_throttle (gboolean throttle);
+
+#endif	/* _GPMSCREENSAVER_H */
