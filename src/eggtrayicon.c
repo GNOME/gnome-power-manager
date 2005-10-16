1365e8f8403de36411f98e6a60d82f3bc7fd53f7
Richard Hughes <rhughes@src.gnome.org>
2005-10-16 22:59:16 +0000

    Make doxygen ignore these files.

--- src/eggtrayicon.c
+++ src/eggtrayicon.c
@@ -1,20 +1,30 @@
-/* eggtrayicon.c
- * Copyright (C) 2002 Anders Carlsson <andersca@gnu.org>
+/*! @file	eggtrayicon.c
+ *  @brief	EggTrayIcon low level hooks.
+ *  @author	Anders Carlsson <andersca@gnu.org>
+ *  @date	2002
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ *  @todo	Don't use this file. Use the sexy new stuff in CVS gtk.
+ */
+/*
+ * Licensed under the GNU General Public License Version 2
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
  */
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
 
 #include <config.h>
 #include <string.h>
@@ -495,3 +505,5 @@ egg_tray_icon_get_orientation (EggTrayIcon * icon)
 
 	return icon->orientation;
 }
+
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
--- src/eggtrayicon.h
+++ src/eggtrayicon.h
@@ -1,20 +1,30 @@
-/* eggtrayicon.h
- * Copyright (C) 2002 Anders Carlsson <andersca@gnu.org>
+/*! @file	eggtrayicon.h
+ *  @brief	EggTrayIcon low level hook header.
+ *  @author	Anders Carlsson <andersca@gnu.org>
+ *  @date	2002
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ *  @todo	Don't use this file. Use the sexy new stuff in CVS gtk.
+ */
+/*
+ * Licensed under the GNU General Public License Version 2
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
  */
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
 
 #ifndef __EGG_TRAY_ICON_H__
 #define __EGG_TRAY_ICON_H__
@@ -56,3 +66,5 @@ GtkOrientation egg_tray_icon_get_orientation (EggTrayIcon * icon);
 
 G_END_DECLS
 #endif				/* __EGG_TRAY_ICON_H__ */
+
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
