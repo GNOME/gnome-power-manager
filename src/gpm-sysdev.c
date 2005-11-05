f78709a3dc01e4ff8ca3ad1b5d3de74d7830ac6a
Richard Hughes <rhughes@src.gnome.org>
2005-11-05 23:39:16 +0000

    Add new system device structures

---
+++ src/gpm-sysdev.c
@@ -0,0 +1,347 @@
+/** @file	gpm-sysdev.c
+ *  @brief	The system device store
+ *  @author	Richard Hughes <richard@hughsie.com>
+ *  @date	2005-11-05
+ *
+ * This module handles the abstraction of the 'system struct' into
+ * 'system devices'. This complexity is required as a user may have
+ * more than one laptop battery or UPS (or misc. wireless periperial).
+ * This is required as we show the icons and do the events as averaged
+ * over all battery devices of the same type.
+ */
+/*
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <glib.h>
+#include <gnome.h>
+#include <string.h>
+#include "gpm-sysdev.h"
+#include "compiler.h"
+
+static sysDev mysystem[BATT_LAST];
+
+/** Converts a DeviceType to a string
+ *
+ *  @param	type		The device type
+ *  @return			The string, e.g. "battery"
+ */
+gchar *
+sysDevToString (DeviceType type)
+{
+	if (type == BATT_PRIMARY)
+		return _("Laptop battery");
+	else if (type == BATT_UPS)
+		return _("UPS");
+	else if (type == BATT_MOUSE)
+		return _("Wireless mouse");
+	else if (type == BATT_KEYBOARD)
+		return _("Wireless keyboard");
+	else if (type == BATT_PDA)
+		return _("Misc PDA");
+	g_error ("Type '%i' not recognised", type);
+	return NULL;
+}
+
+/** Returns the sysDev from the internal static device store.
+ *
+ *  @param	type		The device type
+ *  @return			The sysDev
+ *
+ *  @note	Nowhere in the program other than this should
+ *		the mysystem[type] store be accessed.
+ */
+sysDev *
+sysDevGet (DeviceType type)
+{
+	if (type == BATT_PRIMARY ||
+	    type == BATT_MOUSE ||
+	    type == BATT_KEYBOARD ||
+	    type == BATT_PDA ||
+	    type == BATT_UPS)
+		return &mysystem[type];
+	g_error ("sysDevGet!");
+	return NULL;
+}
+
+/** Initialises the system device object of a specified type
+ *
+ *  @param	type		The device type
+ */
+void
+sysDevInit (DeviceType type)
+{
+	sysDev *sd = sysDevGet (type);
+	sd->devices = g_ptr_array_new ();
+	sd->percentageCharge = 0;
+	sd->minutesRemaining = 0;
+}
+
+/** Frees the system device object of a specified type
+ *
+ *  @param	type		The device type
+ */
+void
+sysDevFree (DeviceType type)
+{
+	sysDev *sd = sysDevGet (type);
+	g_ptr_array_free (sd->devices, TRUE);
+	sd->devices = NULL;
+	sd->percentageCharge = 0;
+	sd->minutesRemaining = 0;
+}
+
+/** Prints the system device object of a specified type
+ *
+ *  @param	type		The device type
+ */
+void
+sysDevPrint (DeviceType type)
+{
+	sysDev *sd = sysDevGet (type);
+	g_print ("Printing %s device parameters:\n", sysDevToString (type));
+	g_print ("percentageCharge = %i\n", sd->percentageCharge);
+	g_print ("minutesRemaining = %i\n", sd->minutesRemaining);
+	g_print ("numberDevices    = %i\n", sd->numberDevices);
+}
+
+/** Initialises all the system device objects
+ */
+void
+sysDevInitAll ()
+{
+	g_debug ("Initing all device types");
+	sysDevInit (BATT_PRIMARY);
+	sysDevInit (BATT_UPS);
+	sysDevInit (BATT_MOUSE);
+	sysDevInit (BATT_KEYBOARD);
+	sysDevInit (BATT_PDA);
+}
+
+/** Frees all the system device objects
+ */
+void
+sysDevFreeAll (DeviceType type)
+{
+	g_debug ("Freeing all device types");
+	sysDevFree (BATT_PRIMARY);
+	sysDevFree (BATT_UPS);
+	sysDevFree (BATT_MOUSE);
+	sysDevFree (BATT_KEYBOARD);
+	sysDevFree (BATT_PDA);
+}
+
+/** Prints all the system device objects
+ */
+void
+sysDevPrintAll (void)
+{
+	sysDev *sd;
+	g_debug ("Printing all device types");
+	/* only print if we have at least one device type */
+	sd = sysDevGet (BATT_PRIMARY);
+	if (sd->numberDevices > 0)
+		sysDevPrint (BATT_PRIMARY);
+	sd = sysDevGet (BATT_UPS);
+	if (sd->numberDevices > 0)
+		sysDevPrint (BATT_UPS);
+	sd = sysDevGet (BATT_MOUSE);
+	if (sd->numberDevices > 0)
+		sysDevPrint (BATT_MOUSE);
+	sd = sysDevGet (BATT_KEYBOARD);
+	if (sd->numberDevices > 0)
+		sysDevPrint (BATT_KEYBOARD);
+	sd = sysDevGet (BATT_PDA);
+	if (sd->numberDevices > 0)
+		sysDevPrint (BATT_PDA);
+}
+
+/** Updates all the system device objects
+ *
+ *  @param	type		The device type
+ *  @return			Success
+ */
+void
+sysDevUpdateAll (void)
+{
+	g_debug ("Updating all device types");
+	sysDevUpdate (BATT_PRIMARY);
+	sysDevUpdate (BATT_UPS);
+	sysDevUpdate (BATT_MOUSE);
+	sysDevUpdate (BATT_KEYBOARD);
+	sysDevUpdate (BATT_PDA);
+}
+
+
+/** Adds a system struct to a system device
+ *
+ *  @param	type		The device type
+ *  @return			Success
+ */
+void
+sysDevAdd (DeviceType type, sysDevStruct *sds)
+{
+	g_assert (sds);
+
+	/* need to check if already exists */
+	sysDev *sd = sysDevGet (type);
+	g_assert (sd);
+	g_assert (sd->devices);
+	g_ptr_array_add (sd->devices, (gpointer) sds);
+	sd->numberDevices++;
+}
+
+/** Prints all the system structs of a specified system device
+ *
+ *  @param	type		The device type
+ *  @return			Success
+ */
+void
+sysDevList (DeviceType type)
+{
+	int a;
+	sysDev *sd = sysDevGet (type);
+	sysDevStruct *temp;
+
+	g_print ("Printing %s device list:\n", sysDevToString (type));
+	for (a=0; a < sd->devices->len; a++) {
+		temp = (sysDevStruct *) g_ptr_array_index (sd->devices, a);
+		g_print ("%s (%i)\n", temp->udi, a);
+		g_print (" percentageCharge : %i\n", temp->percentageCharge);
+		g_print (" minutesRemaining    : %i\n", temp->minutesRemaining);
+		g_print (" present        : %i\n", temp->present);
+	}
+}
+
+/** Finds all the system structs of a specified system device
+ *
+ *  @param	type		The device type
+ *  @param	udi		The HAL UDI
+ *  @return			A sysDevStruct
+ */
+sysDevStruct *
+sysDevFind (DeviceType type, const gchar *udi)
+{
+	int a;
+	sysDev *sd = sysDevGet (type);
+	sysDevStruct *temp;
+
+	if (!udi) {
+		g_warning ("sysDevFind uni is NULL");
+		return NULL;
+	}
+
+	for (a=0; a < sd->devices->len; a++) {
+		temp = (sysDevStruct *) g_ptr_array_index (sd->devices, a);
+		if (strcmp(temp->udi, udi) == 0)
+			return temp;
+	}
+	return NULL;
+}
+
+/** Finds all the system structs of any system device
+ *
+ *  @param	type		The device type
+ *  @param	udi		The HAL UDI
+ *  @return			A sysDevStruct
+ */
+sysDevStruct *
+sysDevFindAll (const gchar *udi)
+{
+	sysDevStruct *temp;
+	/* ordered in order of likeliness */
+	temp = sysDevFind (BATT_PRIMARY, udi);
+	if (temp)
+		return temp;
+	temp = sysDevFind (BATT_MOUSE, udi);
+	if (temp)
+		return temp;
+	temp = sysDevFind (BATT_KEYBOARD, udi);
+	if (temp)
+		return temp;
+	temp = sysDevFind (BATT_UPS, udi);
+	if (temp)
+		return temp;
+	temp = sysDevFind (BATT_PDA, udi);
+	if (temp)
+		return temp;
+	return NULL;
+}
+
+/** Removes the system struct from a specified system device
+ *
+ *  @param	type		The device type
+ *  @param	udi		The HAL UDI
+ */
+void
+sysDevRemove (DeviceType type, const char *udi)
+{
+	sysDev *sd = sysDevGet (type);
+	sysDevStruct *device = sysDevFind (type, udi);
+	if (!device) {
+		g_warning ("'%s' not found!", udi);
+		return;
+	}
+	sd->numberDevices--;
+	g_ptr_array_remove (sd->devices, device);
+	g_free (device);
+}
+
+/** Removes the system struct from any system device
+ *
+ *  @param	udi		The HAL UDI
+ */
+void
+sysDevRemoveAll (const char *udi)
+{
+	sysDevRemove (BATT_PRIMARY, udi);
+	sysDevRemove (BATT_UPS, udi);
+	sysDevRemove (BATT_MOUSE, udi);
+	sysDevRemove (BATT_KEYBOARD, udi);
+	sysDevRemove (BATT_PDA, udi);
+}
+
+/** Updates the system device object of a specified type
+ *
+ *  @param	type		The device type
+ */
+void
+sysDevUpdate (DeviceType type)
+{
+	int a;
+	sysDev *sd = sysDevGet (type);
+	sysDevStruct *temp;
+
+	/* clear old values */
+	sd->minutesRemaining = 0;
+	sd->percentageCharge = 0;
+
+	/* iterate thru all the devices */
+	for (a=0; a < sd->devices->len; a++) {
+		temp = (sysDevStruct *) g_ptr_array_index (sd->devices, a);
+		/* for now, just add */
+		sd->minutesRemaining += temp->minutesRemaining;
+		/* for now, just average */
+		sd->percentageCharge += (temp->percentageCharge/sd->numberDevices);
+	}
+
+}
---
+++ src/gpm-sysdev.h
@@ -0,0 +1,86 @@
+/** @file	gpm-sysdev.h
+ *  @brief	The system device store
+ *  @author	Richard Hughes <richard@hughsie.com>
+ *  @date	2005-11-05
+ */
+/*
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+#ifndef _GPMSYSDEV_H
+#define _GPMSYSDEV_H
+
+/** The different types of battery */
+typedef enum {
+	BATT_PRIMARY,			/**< devices of type primary (laptop)	*/
+	BATT_UPS,			/**< devices of type UPS		*/
+	BATT_MOUSE,			/**< devices of type mouse		*/
+	BATT_KEYBOARD,			/**< devices of type keyboard		*/
+	BATT_PDA,			/**< devices of type PDA		*/
+	BATT_LAST			/**< The last device, for the array #	*/
+} DeviceType;
+
+/**
+ * The 'system struct'
+ *
+ * Every laptop battery, mouse, ups keyboard has one of these
+ * with a unique UDI
+ */
+typedef struct {
+	gchar udi[128];		/**< The HAL UDI			*/
+	gboolean isRechargeable;/**< If device is rechargeable		*/
+	int percentageCharge;	/**< The percentage charge remaining	*/
+	int minutesRemaining;	/**< Minutes remaining until charged	*/
+	gboolean present;	/**< If the device is present		*/
+	gboolean isCharging;	/**< If device is charging		*/
+	gboolean isDischarging;	/**< If device is discharging		*/
+} sysDevStruct;
+
+/**
+ * The 'system device'
+ *
+ * Multiple batteries percentages are averaged and times added
+ * so that a virtual device is presented to the program. This is
+ * required as we show the icons and do the events as averaged
+ * over all battery devices of the same type.
+ */
+typedef struct {
+	int percentageCharge;	/**< The percentage charge remaining	*/
+	int minutesRemaining;	/**< Minutes remaining until charged	*/
+	int numberDevices;	/**< Number of devices of this type	*/
+	GPtrArray* devices;	/**< system struct array		*/
+} sysDev;
+
+gchar *sysDevToString (DeviceType type);
+
+sysDev *sysDevGet (DeviceType type);
+sysDevStruct *sysDevFind (DeviceType type, const gchar *udi);
+sysDevStruct *sysDevFindAll (const gchar *udi);
+
+void sysDevInitAll ();
+void sysDevFreeAll ();
+void sysDevPrintAll (void);
+void sysDevUpdateAll ();
+
+void sysDevList (DeviceType type);
+void sysDevAdd (DeviceType type, sysDevStruct *sds);
+void sysDevPrint (DeviceType type);
+void sysDevUpdate (DeviceType type);
+void sysDevRemove (DeviceType type, const char *udi);
+void sysDevRemoveAll (const char *udi);
+
+#endif	/* _GPMSYSDEV_H */
