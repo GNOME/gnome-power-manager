bc36feefe9e73633d6ecd8ac76e956c3c1ec2bb9
Richard Hughes <rhughes@src.gnome.org>
2005-12-29 11:12:08 +0000

    Add "Requires: dbus-x11" to fix RH#176656

--- src/gpm-console.c
+++ src/gpm-console.c
@@ -54,10 +54,6 @@ gboolean isVerbose;
 
 int daemon (int nochdir, int noclose);
 
-/** Generic exit
- *
- */
-
 static void
 gpm_console_debug_log_ignore (const gchar *log_domain, GLogLevelFlags log_level, const gchar *message, gpointer user_data)
 {
