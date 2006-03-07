6df2d4092d35929b19e82c6ae1a5eb97e1ab129b
Richard Hughes <richard@hughsie.com>
2006-03-07 18:57:27 +0000

    Check for cairo. This is an optional dependency if you want to build the
    2006-03-07  Richard Hughes  <richard@hughsie.com>
     * configure.in: Check for cairo. This is an optional dependency if you want to build the experimental graph support.
     * data/gpm-info.glade: Don't set the size of the graph in the glade file, instead use the source.
     * src/gpm-graph-widget.c: Add the patch from Alexander Larsson so we don't antialias the gridlines. Also do lots of misc fixes to make this grid-thing work as it should.
     * src/gpm-info.c: Make some more stuff generic, and make the graph update every hardware interval. Also free our dynamically allocated memory when the [x] is clicked.
     This means that is you compile with --enable-experimental-features then you will get the graph. Do not do this on production systems as it's crashed once or twice, probably has spelling errors, and certainly the glade file could do with some HIG love. But it looks way cool.

--- src/gpm-graph-widget.c
+++ src/gpm-graph-widget.c
@@ -7,6 +7,7 @@
  *
  */
 
+#include "config.h"
 #include <gtk/gtk.h>
 #include <math.h>
 
@@ -15,9 +16,6 @@
 
 G_DEFINE_TYPE (GpmSimpleGraph, gpm_simple_graph, GTK_TYPE_DRAWING_AREA);
 #define GPM_SIMPLE_GRAPH_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), GPM_TYPE_SIMPLE_GRAPH, GpmSimpleGraphPrivate))
-
-/* THIS IS A BODGE UNTIL WE CHECK FOR CAIRO */
-#define USE_CAIRO	FALSE
 
 struct GpmSimpleGraphPrivate
 {
@@ -99,7 +97,7 @@ gpm_simple_graph_set_data (GpmSimpleGraph *graph, GList *list)
 	graph->priv->list = list;
 }
 
-#if USE_CAIRO
+#ifdef HAVE_CAIRO
 static char *
 get_hour (int totalminutes)
 {
@@ -113,28 +111,29 @@ static void
 draw_grid (GpmSimpleGraph *graph, cairo_t *cr)
 {
 	int a, b;
+	double dotted[] = {1., 2.};
 	double divwidth  = graph->priv->box_width / 10;
 	double divheight = graph->priv->box_height / 10;
 
 	cairo_save (cr); /* push stack */
 
-	cairo_set_line_width (cr, 0.4 * cairo_get_line_width (cr));
+	cairo_set_line_width (cr, 1);
 	cairo_set_dash (cr, dotted, 2, 0.0);
 
 	/* do vertical lines */
 	cairo_set_source_rgb (cr, 0.1, 0.1, 0.1);
 	for (a=1; a<10; a++) {
 		b = graph->priv->box_x + (a * divwidth);
-		cairo_move_to (cr, b, graph->priv->box_y);
-		cairo_line_to (cr, b, graph->priv->box_y + graph->priv->box_height);		
+		cairo_move_to (cr, b + 0.5, graph->priv->box_y);
+		cairo_line_to (cr, b + 0.5, graph->priv->box_y + graph->priv->box_height);		
 		cairo_stroke (cr);
 	}
 
 	/* do horizontal lines */
 	for (a=1; a<10; a++) {
 		b = graph->priv->box_y + (a * divheight);
-		cairo_move_to (cr, graph->priv->box_x, b);
-		cairo_line_to (cr, graph->priv->box_x + graph->priv->box_width, b);		
+		cairo_move_to (cr, graph->priv->box_x, b + 0.5);
+		cairo_line_to (cr, graph->priv->box_x + graph->priv->box_width, b + 0.5);		
 		cairo_stroke (cr);
 	}
 
@@ -196,9 +195,11 @@ static void
 draw_line (GpmSimpleGraph *graph, cairo_t *cr)
 {
 	int a;
+	double unitx = graph->priv->box_width / 100.f;
+	double unity = graph->priv->box_height / 100.f;
 
 	if (! graph->priv->list) {
-		g_warning ("no data");
+		gpm_debug ("no data");
 		return;
 	}
 
@@ -211,6 +212,12 @@ draw_line (GpmSimpleGraph *graph, cairo_t *cr)
 	
 	for (a=1; a<=g_list_length (graph->priv->list)-1; a++) {
 		new = (GpmSimpleDataPoint *) g_list_nth_data (graph->priv->list, a);
+		if (new->x < 0 || new->x > 100) {
+			new->x = 50;
+		}
+		if (new->y < 0 || new->y > 100) {
+			new->y = 50;
+		}
 		cairo_move_to (cr,
 			       graph->priv->box_x + (unitx * old->x),
 			       graph->priv->box_y + (unity * (100 - old->y)));
@@ -236,12 +243,12 @@ draw_graph (GtkWidget *graph_widget, cairo_t *cr)
 
 	GpmSimpleGraph *graph = (GpmSimpleGraph*) graph_widget;
 
+	cairo_save (cr); /* push stack */
+
 	graph->priv->box_x = graph_widget->allocation.x + 40;
 	graph->priv->box_y = graph_widget->allocation.y + 5;
 	graph->priv->box_width = graph_widget->allocation.width - (5 + 40);
 	graph->priv->box_height = graph_widget->allocation.height - (5 + 20);
-
-	cairo_save (cr); /* push stack */
 
 	/* background */
 	cairo_rectangle (cr, graph->priv->box_x, graph->priv->box_y,
@@ -256,10 +263,10 @@ draw_graph (GtkWidget *graph_widget, cairo_t *cr)
 	draw_line (graph, cr);
 
 	/* solid outline box */
-	cairo_rectangle (cr, graph->priv->box_x, graph->priv->box_y,
-			 graph->priv->box_width, graph->priv->box_height);
+	cairo_rectangle (cr, graph->priv->box_x + 0.5, graph->priv->box_y + 0.5,
+			 graph->priv->box_width - 1 , graph->priv->box_height - 1);
 	cairo_set_source_rgb (cr, 0.1, 0.1, 0.1);
-	cairo_set_line_width (cr, 2);
+	cairo_set_line_width (cr, 1);
 	cairo_stroke (cr);
 
 	cairo_restore (cr); /* pop stack */
@@ -269,16 +276,20 @@ draw_graph (GtkWidget *graph_widget, cairo_t *cr)
 static gboolean
 gpm_simple_graph_expose (GtkWidget *graph, GdkEventExpose *event)
 {
-#if USE_CAIRO
+#ifdef HAVE_CAIRO
 	cairo_t *cr;
 
 	/* get a cairo_t */
 	cr = gdk_cairo_create (graph->window);
 
 	cairo_rectangle (cr,
-			event->area.x, event->area.y,
-			event->area.width, event->area.height);
+			 event->area.x, event->area.y,
+			 event->area.width, event->area.height);
 	cairo_clip (cr);
+
+	/* not sure why, maybe bug in gtk? */
+	graph->allocation.x = event->area.x;
+	graph->allocation.y = event->area.y;
 
 	draw_graph (graph, cr);
 
--- src/gpm-info.c
+++ src/gpm-info.c
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
  *
- * Copyright (C) 2005-2006 Richard Hughes <richard@hughsie.com>
+ * Copyright (C) 2006 Richard Hughes <richard@hughsie.com>
  *
  * Licensed under the GNU General Public License Version 2
  *
@@ -38,15 +38,19 @@ static void     gpm_info_finalize   (GObject      *object);
 
 #define GPM_INFO_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), GPM_TYPE_INFO, GpmInfoPrivate))
 
-#define GPM_INFO_POLL		60
+#define GPM_INFO_HARDWARE_POLL		10 /* seconds */
+#define GPM_INFO_DATA_RES_X		40 /* x resolution, greater burns the cpu */
+//#define DO_TESTING			TRUE
 
 struct GpmInfoPrivate
 {
 	GpmPower	*power;
 
 	GList		*log_percentage_charge;
+	GList		*graph_percentage_charge;
 
 	GtkWidget	*main_window;
+	GtkWidget	*graph_percentage;
 };
 
 enum {
@@ -72,31 +76,27 @@ gpm_graph_custom_handler (GladeXML *xml,
 			  gint int1, gint int2,
 			  gpointer user_data)
 {
-	GpmInfo *info;
 	GtkWidget *widget = NULL;
-
-	info = (GpmInfo *) user_data;
-
 	if (strcmp ("gpm_simple_graph_new", func_name) == 0) {
 		widget = gpm_simple_graph_new ();
 		return widget;
 	}
-
 	return NULL;
 }
 
 static void
-gpm_stat_add_data_point (GList *list, int x, int y)
+gpm_info_data_point_add (GList **list, int x, int y)
 {
 	GpmSimpleDataPoint *data_point;
 	data_point = g_new (GpmSimpleDataPoint, 1);
 	data_point->x = x;
 	data_point->y = y;
-	list = g_list_append (list, (gpointer) data_point);
+	*list = g_list_append (*list, (gpointer) data_point);
 }
 
+/* normalise 0..100 */
 static void
-gpm_stat_calculate_percentage (GList *source, GList *destination, int num_points)
+gpm_stat_calculate_percentage (GList *source, GList **destination, int num_points)
 {
 	int count = 0;		/* what number of max_count we are at */
 	int max_count;		/* how many data values do we want */
@@ -106,7 +106,6 @@ gpm_stat_calculate_percentage (GList *source, GList *destination, int num_points
 	int a;
 	int *point;
 	float percentage_step = 100.0f / (float) num_points;
-
 	max_count = g_list_length (source) / num_points;
 
 	for (a=0; a < g_list_length (source) - 1; a++) {
@@ -115,8 +114,8 @@ gpm_stat_calculate_percentage (GList *source, GList *destination, int num_points
 		if (count == max_count) {
 			value_y = (float) average_add / (float) (max_count + 1);
 			value_x = (int) (percentage_step * (float) this_count);
-			gpm_debug ("(%i) value = (%i, %i)", this_count, value_x, value_y);
-			gpm_stat_add_data_point (destination, value_x, value_y);
+//			gpm_debug ("(%i) value = (%i, %i)", this_count, value_x, value_y);
+			gpm_info_data_point_add (destination, value_x, value_y);
 			count = 0;
 			average_add = 0;
 			this_count++;
@@ -124,12 +123,8 @@ gpm_stat_calculate_percentage (GList *source, GList *destination, int num_points
 			count++;
 		}
 	}
-	if (a > num_points && num_points != this_count) {
-		value_y = (float) average_add / (float) count;
-		value_x = 100;
-		gpm_debug ("    value = (%i, %i)", value_x, value_y);
-		gpm_stat_add_data_point (destination, value_x, value_y);
-	}
+	/* we do not calculate the average on the remainder as the sample size would
+	   not be equal, and thus not representative of the final value */
 }
 
 static void
@@ -151,6 +146,66 @@ gpm_info_close_cb (GtkWidget	*widget,
 {
 	gtk_widget_destroy (info->priv->main_window);
 	info->priv->main_window = NULL;
+}
+
+static void
+gpm_info_data_point_free (GList **list)
+{
+	if (list == NULL || *list == NULL) {
+		return;
+	}
+	int a;
+	GpmSimpleDataPoint *d_point;
+
+	/* free elements */
+	for (a=0; a<g_list_length (*list); a++) {
+		d_point = (GpmSimpleDataPoint*) g_list_nth_data (*list, a);
+		g_free (d_point);
+	}
+	g_list_free (*list);
+	*list = NULL;
+}
+
+static void
+gpm_info_data_point_print (GList *list)
+{
+	int a;
+	GpmSimpleDataPoint *d_point;
+	/* print graph data */
+	gpm_debug ("GRAPH DATA");
+	for (a=0; a<g_list_length (list); a++) {
+		d_point = (GpmSimpleDataPoint*) g_list_nth_data (list, a);
+		gpm_debug ("data %i = (%i, %i)", a, d_point->x, d_point->y);
+	}
+}
+
+static void
+gpm_info_update_graph (GpmInfo *info)
+{
+	GtkWidget *widget = info->priv->graph_percentage;
+	int max_time;
+
+	/* free existing data if exists */
+	gpm_info_data_point_free (&(info->priv->graph_percentage_charge));
+
+	if (info->priv->log_percentage_charge) {
+		gpm_stat_calculate_percentage (info->priv->log_percentage_charge,
+					       &(info->priv->graph_percentage_charge),
+					       GPM_INFO_DATA_RES_X + 1);
+		gpm_simple_graph_set_data (GPM_SIMPLE_GRAPH (widget), info->priv->graph_percentage_charge);
+		max_time = (GPM_INFO_HARDWARE_POLL * g_list_length (info->priv->log_percentage_charge)) / 60;
+		if (max_time == 0) {
+			max_time = 10;
+		}
+		gpm_simple_graph_set_stop_x (GPM_SIMPLE_GRAPH (widget), max_time);
+	} else {
+		gpm_debug ("no log data");
+	}
+
+	gtk_widget_set_size_request (widget, 600, 300);
+
+	gtk_widget_hide (widget);
+	gtk_widget_show (widget);
 }
 
 void
@@ -177,6 +232,9 @@ gpm_info_show_window (GpmInfo *info)
 	gtk_widget_hide (info->priv->main_window);
 	gtk_window_set_icon_name (GTK_WINDOW (info->priv->main_window), "gnome-dev-battery");
 
+	g_signal_connect (info->priv->main_window, "delete_event",
+			  G_CALLBACK (gpm_info_close_cb), info);
+
 	widget = glade_xml_get_widget (glade_xml, "button_close");
 	g_signal_connect (widget, "clicked",
 			  G_CALLBACK (gpm_info_close_cb), info);
@@ -185,30 +243,12 @@ gpm_info_show_window (GpmInfo *info)
 	g_signal_connect (widget, "clicked",
 			  G_CALLBACK (gpm_info_help_cb), info);
 
+	info->priv->graph_percentage = glade_xml_get_widget (glade_xml, "graph_percentage");
+
+	gpm_info_update_graph (info);
+	gpm_info_data_point_print (info->priv->graph_percentage_charge);
+
 	gtk_widget_show (info->priv->main_window);
-
-	int a;
-	GpmSimpleDataPoint *d_point;
-
-	GList *values = NULL;
-
-	if (info->priv->log_percentage_charge) {
-		gpm_stat_calculate_percentage (info->priv->log_percentage_charge, values, 11);
-	}
-
-	/* print graph data */
-	gpm_debug ("GRAPH DATA");
-	for (a=0; a<g_list_length (values); a++) {
-		d_point = (GpmSimpleDataPoint*) g_list_nth_data (values, a);
-		gpm_debug ("data %i = (%i, %i)", a, d_point->x, d_point->y);
-	}
-
-	/* free elements */
-	for (a=0; a<g_list_length (values); a++) {
-		d_point = (GpmSimpleDataPoint*) g_list_nth_data (values, a);
-		g_free (d_point);
-	}
-	g_list_free (values);
 }
 
 /* log data every minute */
@@ -224,9 +264,23 @@ log_do_poll (gpointer data)
 				      &battery_status);
 	point = g_new (int, 1);
 	*point = battery_status.percentage_charge;
-	gpm_debug ("Graph point %i", *point);
+
+#ifdef DO_TESTING
+	static int auto_inc_val = 0;
+	auto_inc_val++;
+	if (auto_inc_val == 100) {
+		auto_inc_val = 0;
+	}
+	*point += auto_inc_val;
+#endif
+
+	gpm_debug ("logging percentage_charge: %i", *point);
 	info->priv->log_percentage_charge = g_list_append (info->priv->log_percentage_charge,
 							   (gpointer) point);
+	if (info->priv->main_window) {
+		gpm_info_update_graph (info);
+		gpm_info_data_point_print (info->priv->graph_percentage_charge);
+	}
 	return TRUE;
 }
 
@@ -254,7 +308,25 @@ gpm_info_init (GpmInfo *info)
 	info->priv->main_window = NULL;
 	/* set up the timer callback so we can log data every minute */
 	info->priv->log_percentage_charge = NULL;
-	g_timeout_add (GPM_INFO_POLL * 1000, log_do_poll, info);
+	info->priv->graph_percentage_charge = NULL;
+	g_timeout_add (GPM_INFO_HARDWARE_POLL * 1000, log_do_poll, info);
+}
+
+static void
+gpm_info_log_free (GList **list)
+{
+	if (list == NULL || *list == NULL) {
+		return;
+	}
+	int *data;
+	int a;
+	for (a=0; a<g_list_length (*list); a++) {
+		data = (int*) g_list_nth_data (*list, a);
+		g_free (data);
+	}
+	g_list_free (*list);
+	*list = NULL;
+
 }
 
 static void
@@ -268,13 +340,8 @@ gpm_info_finalize (GObject *object)
 	info->priv = GPM_INFO_GET_PRIVATE (info);
 
 	/* free log_percentage_charge elements */
-	int *data;
-	int a;
-	for (a=0; a<g_list_length (info->priv->log_percentage_charge); a++) {
-		data = (int*) g_list_nth_data (info->priv->log_percentage_charge, a);
-		g_free (data);
-	}
-	g_list_free (info->priv->log_percentage_charge);
+	gpm_info_log_free (&(info->priv->log_percentage_charge));
+	gpm_info_data_point_free (&(info->priv->graph_percentage_charge));
 
 	G_OBJECT_CLASS (parent_class)->finalize (object);
 }
