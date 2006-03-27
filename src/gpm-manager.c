e9ce4293f73b4978c3a1253afa10ad240eec5506
Richard Hughes <richard@hughsie.com>
2006-03-27 17:18:52 +0000

    Don't return GPM_WARNING_ACTION when the battery value is 0% as this *may*
    2006-03-27  Richard Hughes  <richard@hughsie.com>
     * src/gpm-manager.c (gpm_manager_get_warning_type): Don't return GPM_WARNING_ACTION when the battery value is 0% as this *may* happen with broken acpi laptops. Instead warn on the console.

--- src/gpm-manager.c
+++ src/gpm-manager.c
@@ -1238,7 +1238,13 @@ gpm_manager_get_warning_type (GpmPowerBatteryStatus *battery_status,
 			type = GPM_WARNING_LOW;
 		}
 	} else {
-		if (battery_status->percentage_charge <= BATTERY_ACTION_PERCENTAGE) {
+		if (battery_status->percentage_charge <= 0) {
+			type = GPM_WARNING_NONE;
+			gpm_warning ("Your hardware is reporting a percentage "
+				     "charge of %i, which is impossible. "
+				     "WARNING_ACTION will *not* be reported.",
+				     battery_status->percentage_charge);
+		} else if (battery_status->percentage_charge <= BATTERY_ACTION_PERCENTAGE) {
 			type = GPM_WARNING_ACTION;
 		} else if (battery_status->percentage_charge <= BATTERY_CRITICAL_PERCENTAGE) {
 			type = GPM_WARNING_CRITICAL;
