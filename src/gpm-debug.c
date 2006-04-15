9b4bcd2a099ca22669958d882a8157c828d7a044
Dennis Cranston <dennis_cranston@yahoo.com>
2006-04-15 15:57:42 +0000

    Error alerts should use an OK button. Also, fix it so the error dialog can
    2006-04-15  Dennis Cranston  <dennis_cranston@yahoo.com>
    
    	* src/gpm-debug.c: (gpm_critical_error):  Error alerts should use
            an OK button.  Also, fix it so the error dialog can be dismissed.
    	Fixes #336997.

--- src/gpm-debug.c
+++ src/gpm-debug.c
@@ -165,7 +165,7 @@ gpm_debug_init (gboolean debug)
  * gpm_critical_error:
  * @content: The content to show, e.g. "No icons detected"
  *
- * Shows a gtk critical error, logs to syslog and closes the program.
+ * Shows a gtk critical error and logs to syslog.
  * NOTE: we will loose memory, but since this program is a critical error
  * that is the least of our problems...
  **/
@@ -184,19 +184,17 @@ gpm_critical_error (const char *format, ...)
 	dialog = gtk_message_dialog_new_with_markup (NULL,
 						     GTK_DIALOG_MODAL,
 						     GTK_MESSAGE_WARNING,
-						     GTK_BUTTONS_CLOSE,
+						     GTK_BUTTONS_OK,
 						     "<span size='larger'><b>%s</b></span>",
 						     GPM_NAME);
 	gtk_message_dialog_format_secondary_markup (GTK_MESSAGE_DIALOG (dialog),
 						    buffer);
-	/* we close the gtk lopp when the user clicks close */
+	/* we close the gtk loop when the user clicks ok */
 	g_signal_connect_swapped (dialog,
 				  "response",
-				  G_CALLBACK (gtk_main_quit),
-				  NULL);
-	gtk_window_present (GTK_WINDOW (dialog));
-	/* we wait here for user to click close */
-	gtk_main();
+				  G_CALLBACK (gtk_widget_destroy),
+				  dialog);
+	gtk_widget_show (dialog);
 }
 
 /**
