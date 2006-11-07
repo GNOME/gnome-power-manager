b14a7512959771b8129a4a69efc001e4df30d1b3
Richard Hughes <richard@hughsie.com>
2006-11-07 19:46:46 +0000

    Make the shutdown when critical levels a little less critical, as we might
    2006-11-07  Richard Hughes <richard@hughsie.com>
    
    * data/gnome-power-manager.schemas.in:
    Make the shutdown when critical levels a little less critical,
    as we might not have enough power to hibernate. Fixes #369529.
    
    * src/gpm-manager.c: (update_dpms_throttle):
    When we add a DPMS state throttle, remove the last one if set.
    This fixes the bug where the screensaver would not show if
    DPMS had been allowed to activate. Fixes #355279.

--- src/gpm-manager.c
+++ src/gpm-manager.c
@@ -680,6 +680,10 @@ update_dpms_throttle (GpmManager *manager,
 			manager->priv->dpms_throttle_id = 0;
 		}
 	} else {
+		/* if throttle already exists then remove */
+		if (manager->priv->dpms_throttle_id > 0) {
+			gpm_screensaver_remove_throttle (manager->priv->screensaver, manager->priv->dpms_throttle_id);
+		}
 		manager->priv->dpms_throttle_id = gpm_screensaver_add_throttle (manager->priv->screensaver, _("Display power management activated"));
 	}
 }
@@ -2863,6 +2867,8 @@ gpm_manager_init (GpmManager *manager)
 	/* Do we ignore inhibit requests? */
 	manager->priv->ignore_inhibits = gconf_client_get_bool (manager->priv->gconf_client,
 								GPM_PREF_IGNORE_INHIBITS, NULL);
+	/* set no throttle */
+	manager->priv->dpms_throttle_id = -1;
 }
 
 /**
