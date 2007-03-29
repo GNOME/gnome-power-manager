cc2d11533608763532b70ec869d0f991aa20c091
Richard Hughes <richard@hughsie.com>
2007-03-29 09:43:11 +0000

    Generate and ship the 48x48 icons.
    2007-03-29  Richard Hughes  <richard@hughsie.com>
    
    * configure.in:
    * data/icons/48x48/Makefile.am:
    * data/icons/Makefile.am:
    * data/icons/generate_48x48_icons.sh:
    Generate and ship the 48x48 icons.
    
    svn path=/trunk/; revision=2109

---
+++ data/icons/generate_48x48_icons.sh
@@ -0,0 +1,3 @@
+cd scalable
+for i in *.svg;do inkscape --without-gui --export-png="../48x48/"$( echo $i | cut -d . -f -1 ).png --export-dpi=72 --export-background-opacity=0 --export-width=48 --export-height=48 "$i";done
+
