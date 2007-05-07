bec6136ad314f91e2458b17348cbeaab4f86edc8
Richard Hughes <richard@hughsie.com>
2007-05-07 22:21:05 +0000

    Don't chicken out when brightness access fails in the addon. Continue to
    2007-05-07  Richard Hughes  <richard@hughsie.com>
    
    * applets/brightness/brightness-applet.c: (update_level):
    Don't chicken out when brightness access fails in the addon.
    Continue to try to connect, hopefully trying to fix #436704.
    
    svn path=/branches/gnome-2-18/; revision=2186

--- applets/brightness/brightness-applet.c
+++ applets/brightness/brightness-applet.c
@@ -254,10 +254,10 @@ update_tooltip (GpmBrightnessApplet *applet)
 static void
 update_level (GpmBrightnessApplet *applet, gboolean get_hw, gboolean set_hw)
 {
-	if (applet->enabled && set_hw) {
+	if (set_hw == TRUE) {
 		applet->enabled = gpm_powermanager_set_brightness_lcd (applet->powermanager, applet->level);
 	}
-	if (get_hw) {
+	if (get_hw == TRUE) {
 		applet->enabled = gpm_powermanager_get_brightness_lcd (applet->powermanager, &applet->level);
 	}
 	if (applet->popup != NULL) {
