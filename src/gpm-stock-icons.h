0ed6e5ed81cc244fc8e131ed34c1f51cf23b48bb
Richard Hughes <richard@hughsie.com>
2007-07-31 15:55:56 +0000

    Use the correct icon for the right click drop down.
    2007-07-31  Richard Hughes  <richard@hughsie.com>
    
    * src/gpm-stock-icons.h:
    Use the correct icon for the right click drop down.
    
    svn path=/trunk/; revision=2411

--- src/gpm-stock-icons.h
+++ src/gpm-stock-icons.h
@@ -31,7 +31,7 @@ G_BEGIN_DECLS
 #define GPM_STOCK_BATTERY_CHARGED		"gpm-primary-charged"
 #define GPM_STOCK_HIBERNATE			"gpm-hibernate"
 #define GPM_STOCK_SUSPEND			"gpm-suspend"
-#define GPM_STOCK_STATISTICS			"gpm-statistics"
+#define GPM_STOCK_STATISTICS			"gnome-power-statistics"
 #define GPM_STOCK_BRIGHTNESS_LCD		"gpm-brightness-lcd"
 #define GPM_STOCK_BRIGHTNESS_KBD		"gpm-brightness-kbd"
 #define GPM_STOCK_INHIBIT			"gpm-inhibit"
