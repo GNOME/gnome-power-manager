2ce37819052eb8acc2916b021a5965df0b91840d
Richard Hughes <richard@hughsie.com>
2007-08-15 00:54:09 +0000

    Make check work again by removing removed files.
    2007-08-15  Richard Hughes  <richard@hughsie.com>
    
    * po/POTFILES.in:
    Make check work again by removing removed files.
    
    * data/Makefile.am:
    Make intltool a little happier.
    
    * src/gdb-gpm.sh:
    * src/massif-gpm.sh:
    * src/profile-gpm.sh:
    * src/valgrind-gpm.sh:
    * src/run-gpm.sh:
    Move into one tool.
    
    svn path=/trunk/; revision=2432

--- src/gdb-gpm.sh
+++
@@ -1,7 +0,0 @@
-export G_DEBUG=fatal_criticals
-killall gnome-power-manager
-echo "run" > /tmp/gdb
-echo "bt" >> /tmp/gdb
-gdb --batch --command=/tmp/gdb --args ./gnome-power-manager --verbose --no-daemon
-rm /tmp/gdb
-
--- src/valgrind-gpm.sh
+++
@@ -1,5 +0,0 @@
-killall gnome-power-manager
-export G_SLICE=always-malloc
-export G_DEBUG=gc-friendly
-valgrind --tool=memcheck --leak-check=full --leak-resolution=high ./gnome-power-manager --verbose --no-daemon --timed-exit &> ./debug-valgrind.log
-
