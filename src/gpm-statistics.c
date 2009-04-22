32f2a204b15468574d975175826206c4c8226316
Richard Hughes <richard@hughsie.com>
2009-04-22 17:19:38 +0100

    Show the processor device if we have capability, not if we have events
    This fixes the bug where we didn't get wakeup data the first time we
    opened gnome-power-statistics. DeviceKit-power only populates the
    data on the second request, and thus g-p-s wasn't showing the info
    in the majority of cases

--- src/gpm-statistics.c
+++ src/gpm-statistics.c
@@ -1596,9 +1596,14 @@ main (int argc, char *argv[])
 
 	last_device = gconf_client_get_string (gconf_client, GPM_CONF_INFO_LAST_DEVICE, NULL);
 
+#if 0
+	/* FIXME: this is only available in DeviceKit 008 */
+	ret = dkp_wakeups_has_capability (wakeups);
+#else
 	/* can we get wakeup data? */
-	i = dkp_wakeups_get_total (wakeups, NULL);
-	if (i > 0) {
+	ret = TRUE;
+#endif
+	if (ret) {
 		GtkTreeIter iter;
 		gtk_list_store_append (list_store_devices, &iter);
 		gtk_list_store_set (list_store_devices, &iter,
