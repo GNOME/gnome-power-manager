3c6978d0944ef0a16358bbcc52d2dae6cec47433
Richard Hughes <richard@hughsie.com>
2010-05-17 18:03:45 +0100

    trivial: Remove some debugging that crept in

--- src/gpm-brightness.c
+++ src/gpm-brightness.c
@@ -518,8 +518,6 @@ gpm_brightness_foreach_screen (GpmBrightness *brightness, GpmXRandROp op)
 
 	g_return_val_if_fail (GPM_IS_BRIGHTNESS (brightness), FALSE);
 
-	return FALSE;
-
 	/* do for each screen */
 	length = brightness->priv->resources->len;
 	for (i=0; i<length; i++) {
