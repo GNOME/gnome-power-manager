987ebaea4b0adbc3c24d0d9e5dec8694b7455d9c
Richard Hughes <richard@hughsie.com>
2010-08-06 15:32:39 +0100

    trivial: fix up a tiny merge conflict

--- src/gpm-idle.c
+++ src/gpm-idle.c
@@ -290,7 +290,7 @@ gpm_idle_set_timeout_dim (GpmIdle *idle, guint timeout)
 
 	g_return_val_if_fail (GPM_IS_IDLE (idle), FALSE);
 
-	idle_time_in_msec = gpm_idletime_get_time (idle->priv->idletime);
+	idle_time_in_msec = egg_idletime_get_time (idle->priv->idletime);
 	timeout_adjusted  = gpm_idle_adjust_timeout_dim (idle_time_in_msec / 1000, timeout);
 	egg_debug ("Current idle time=%lldms, timeout was %us, becomes %us after adjustment",
 		   idle_time_in_msec, timeout, timeout_adjusted);
